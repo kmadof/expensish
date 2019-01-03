4ad3ce8276bd0c4d25c64e934e5a84de3f78cc5d
Krzysztof Madej <krzysztof.madej.1987@gmail.com>
2019-01-03 06:54:13 +0100

    Set CORS on backedn API

--- expensish-api/Expensish.Api/Startup.cs
+++ expensish-api/Expensish.Api/Startup.cs
@@ -26,6 +26,16 @@ namespace Expensish.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            // Add service and create Policy with options
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials());
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,6 +50,8 @@ namespace Expensish.Api
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseCors("CorsPolicy");
 
             app.UseHttpsRedirection();
             app.UseMvc();
