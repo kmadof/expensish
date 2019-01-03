600e47e3810f59646942a8f20edcab28b0282544
Krzysztof Madej <krzysztof.madej.1987@gmail.com>
2019-01-03 06:52:56 +0100

    Added call to backend api

--- expensish-app/src/app/app.component.ts
+++ expensish-app/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'expensish';
+  values: Array<string>;
+  error: string;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get('expensish-api/api/values').subscribe(
+      (resp: Array<string>) => this.values = resp,
+      error => this.error = error // error path
+    );
+  }
+
 }
